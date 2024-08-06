75bab889058b1898a7529b945606b410b1a34ff0
LeoPazEs <estevam.leonardopaz@gmail.com>
2024-08-06 11:22:14 -0300

    nvim-tree stay in same file
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -10,16 +10,16 @@ return {
     require('nvim-tree').setup {
       disable_netrw = true,
       hijack_netrw = true,
-      open_on_tab = false,
-      hijack_cursor = false,
-      update_cwd = false,
       diagnostics = {
         enable = true,
       },
       update_focused_file = {
-        enable = true,
-        update_cwd = true,
-        ignore_list = {},
+        enable = false,
+        update_root = {
+          enable = false,
+          ignore_list = {},
+        },
+        exclude = false,
       },
       system_open = {
         cmd = nil,

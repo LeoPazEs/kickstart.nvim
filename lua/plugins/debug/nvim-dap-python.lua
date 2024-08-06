8ec3f4d424c8979cc02d418805801a54410055bd
LeoPazEs <estevam.leonardopaz@gmail.com>
2024-08-06 16:20:35 -0300

    debug remote attach to container
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- lua/plugins/debug/nvim-dap-python.lua
+++ lua/plugins/debug/nvim-dap-python.lua
@@ -1,5 +1,6 @@
 return {
   'mfussenegger/nvim-dap-python',
+  dependencies = { 'mfussenegger/nvim-dap' },
   config = function()
     local mason_registry = require 'mason-registry'
     local debugpy = mason_registry.get_package 'debugpy'
@@ -9,5 +10,27 @@ return {
     vim.api.nvim_set_keymap('n', '<leader>dn', '<cmd>lua require("dap-python").test_method()<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>df', '<cmd>lua require("dap-python").test_class()<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', '<leader>ds', '<ESC><cmd>lua require("dap-python").debug_selection()<CR>', { noremap = true, silent = true })
+
+    local dap = require 'dap'
+    table.insert(dap.configurations.python, {
+      type = 'python',
+      request = 'attach',
+      name = 'Attach remote (with path mapping)',
+      mode = 'remote',
+      connect = function()
+        local host = vim.fn.input 'Host [127.0.0.1]: '
+        host = host ~= '' and host or '127.0.0.1'
+        local port = tonumber(vim.fn.input 'Port [5678]: ') or 5678
+        return { host = host, port = port }
+      end,
+      pathMappings = function()
+        local cwd = '${workspaceFolder}'
+        local local_path = vim.fn.input('Local path mapping [' .. cwd .. ']: ')
+        local_path = local_path ~= '' and local_path or cwd
+        local remote_path = vim.fn.input 'Remote path mapping [.]: '
+        remote_path = remote_path ~= '' and remote_path or '.'
+        return { { localRoot = local_path, remoteRoot = remote_path } }
+      end,
+    })
   end,
 }

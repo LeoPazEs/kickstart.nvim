e0f27e3658959f06c96011786b0343d8e84e4294
LeoPazEs <estevam.leonardopaz@gmail.com>
2024-08-06 11:12:53 -0300

    debug method and class
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- lua/plugins/debug/nvim-dap-python.lua
+++ lua/plugins/debug/nvim-dap-python.lua
@@ -6,5 +6,8 @@ return {
     local debugpy_path = debugpy:get_install_path() .. '/venv/bin/python'
 
     require('dap-python').setup(debugpy_path)
+    vim.api.nvim_set_keymap('n', '<leader>dn', '<cmd>lua require("dap-python").test_method()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>df', '<cmd>lua require("dap-python").test_class()<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('v', '<leader>ds', '<ESC><cmd>lua require("dap-python").debug_selection()<CR>', { noremap = true, silent = true })
   end,
 }

e5c55f0c2e7dc7ee224f2698df4128f6b9865034
LeoPazEs <estevam.leonardopaz@gmail.com>
2024-08-06 10:43:24 -0300

    nvim dap python mason debugpy
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- lua/plugins/debug/nvim-dap-python.lua
+++ lua/plugins/debug/nvim-dap-python.lua
@@ -1,4 +1,10 @@
 return {
   'mfussenegger/nvim-dap-python',
-  config = function() end,
+  config = function()
+    local mason_registry = require 'mason-registry'
+    local debugpy = mason_registry.get_package 'debugpy'
+    local debugpy_path = debugpy:get_install_path() .. '/venv/bin/python'
+
+    require('dap-python').setup(debugpy_path)
+  end,
 }

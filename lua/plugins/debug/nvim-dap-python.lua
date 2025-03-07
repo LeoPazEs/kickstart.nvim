fbabb9bd648ad1094decc524d08167154837a49c
leopazes <estevam.leonardopaz@gmail.com>
2025-03-07 11:35:46 -0300

    back to normal because of bugs
    Signed-off-by: leopazes <estevam.leonardopaz@gmail.com>

--- lua/plugins/debug/nvim-dap-python.lua
+++ lua/plugins/debug/nvim-dap-python.lua
@@ -6,7 +6,7 @@ return {
     local debugpy = mason_registry.get_package 'debugpy'
     local debugpy_path = debugpy:get_install_path() .. '/venv/bin/python'
 
-    require('dap-python').setup(debugpy_path, { include_configs = false, console = 'integratedTerminal', pythonPath = nil })
+    require('dap-python').setup(debugpy_path)
     vim.api.nvim_set_keymap('n', '<leader>dn', '<cmd>lua require("dap-python").test_method()<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>df', '<cmd>lua require("dap-python").test_class()<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('v', '<leader>ds', '<ESC><cmd>lua require("dap-python").debug_selection()<CR>', { noremap = true, silent = true })
